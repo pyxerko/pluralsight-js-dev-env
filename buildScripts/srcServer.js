a4582ad5be8a1038212b7f845ea186059d077505
Erik Koch-Nommensen <Erik Koch-Nommensen>
2017-05-08 17:16:52 +0200

    170508

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,22 +1,37 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
-// var absPath = path.join(__dirname, '../src/index.html');
-// console.log('ErKo: ' + absPath);
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-  //res.sendFile(absPath);
+});
+
+app.get('/users', function(req, res){
+  // Hard coding for simplicity. Pretend this is hits a real database
+  res.json([
+    {"id": 1, "firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Tammy","lastName":"Norton","email":"tnorton@yahho.com"},
+    {"id": 3, "firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
 });
 
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
-    // console.log(absPath);
   } else {
     open('http://localhost:' + port);
   }
-})
+});
